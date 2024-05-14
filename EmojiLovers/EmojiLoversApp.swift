a683a90cc0be592b1b7ab4abee0d3205475a81f2
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2024-05-14 13:23:21 +0530

    Initial Commit

---
+++ EmojiLovers/EmojiLoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoversApp.swift
+//  EmojiLovers
+//
+//  Created by Ruchika Sharma on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

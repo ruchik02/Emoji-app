a848d82aee74dd2c0cbb2b022df7689bec37282a
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2024-05-20 16:35:38 +0530

    emoji app

--- EmojiLovers/ContentView.swift
+++ EmojiLovers/ContentView.swift
@@ -1,22 +1,29 @@
-//
-//  ContentView.swift
-//  EmojiLovers
-//
-//  Created by Ruchika Sharma on 14/05/24.
-//
-
 import SwiftUI
+enum Emoji: String, CaseIterable {
+    case 😊, 💞, 🧿, 🚀
+}
 
 struct ContentView: View {
+  @State var selection: Emoji = .🧿
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {
+                        emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.palette)
+            }
+            .navigationTitle("Emoji App")
+            .padding()
+           
         }
-        .padding()
     }
+        
 }
 
 #Preview {
